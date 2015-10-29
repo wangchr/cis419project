bf6a45da4b1630b678ad19bb25a31d84a592f6f7
Christian Wang <wangchr@seas.upenn.edu>
2015-10-29 02:04:43 -0400

    Fixed createAdjMatrix function, now runs without errors

--- src/flavor_network.py
+++ src/flavor_network.py
@@ -8,28 +8,29 @@ class FlavorNetwork:
         Constructor
         '''
 
-    def loadGraph(self):
+    def createAdjMatrix(self):
         # Reads in ingredient pairs and weights from .csv file
         filepath = 'flavor_network_data/srep00196-s2.csv'
         file = open(filepath, 'r')
         data = np.genfromtxt(filepath, dtype = None, delimiter=',', skip_header=4)
-
+self.
         # Assign each unique ingredient an index
         index = 0
+        self.ingred_dict = {}
         for i in xrange(len(data)):
+            ingred = data[i][0]
+            if (not self.ingred_dict.has_key(ingred)):
+                self.ingred_dict[ingred] = index;
+                index += 1
             ingred = data[i][1]
-            if (!ingred_dict.has_key(ingred)):
-                ingred_dict[ingred] = index;
-                index += 1
-            ingred = data[i][2]
-            if (!ingred_dict.has_key(ingred)):
-                ingred_dict[ingred] = index;
+            if (not self.ingred_dict.has_key(ingred)):
+                self.ingred_dict[ingred] = index;
                 index += 1
 
-        # Create adjacency matrix of graph
-        adj_mat = np.zeros([len(dict), len(dict)])
+        # Create adjacency matrix representation of graph
+        n_ingred = len(self.ingred_dict)
+        self.adj_mat = np.zeros([n_ingred,n_ingred])
         for i in xrange(len(data)):
-            ingred1 = data[i][1]
-            ingred2 = data[i][2]
-            adj_mat[ingred_dict[ingred1], ingred_dict[ingred2]] = data[i][3]
-
+            ingred1 = data[i][0]
+            ingred2 = data[i][1]
+            self.adj_mat[self.ingred_dict[ingred1], self.ingred_dict[ingred2]] = data[i][2]
